3b812983a0ad1c60535313119e3eff3e814aeb74
Nicolae <ksirbu519@gmail.com>
2024-06-24 20:14:07 +0300

    fix ts error

--- src/helpers/classnames.ts
+++ src/helpers/classnames.ts
@@ -10,7 +10,7 @@ export function classNames(
     ...additional.filter(Boolean),
 
     ...Object.entries(mods)
-      .filter(([cls, value]) => Boolean(value))
-      .map(([cls, value]) => cls),
+      .filter(([_, value]) => Boolean(value))
+      .map(([cls]) => cls),
   ].join(' ');
 }
